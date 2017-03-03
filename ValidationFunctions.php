bfa0bc7d14a76783a4664c38011123173fdef128
Hshop <hshop@Hshops-MacBook-Pro-2.local>
2017-03-03 10:23:50 +0100

    refactored contains_only_letters to include check if param is string

--- ValidationFunctions.php
+++ ValidationFunctions.php
@@ -4,10 +4,16 @@ class ValidateFunctions
   const ERROR_NAME_NOT_ALPHA = "Navn må kun bestå af bogstaver"; // Virker kun inden i en Class
 
   public function contains_only_letters($name) {
-    if (!preg_match('/^[A-Za-z\s]+$/', $name)) {
-      return false;
+    if (is_string($name)) {
+      // Hvis ikke String kun består af bogstaver og mellemrum return false
+      if (!preg_match('/^[A-Za-z\s]+$/', $name)) {
+        return false;
+      } else {
+        return true;
+      }
     } else {
-      return true;
+      return false;
     }
+
   }
 }
